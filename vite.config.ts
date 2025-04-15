0bf22b039fe902096707c0b25cd51666a56f985b
iraklijanashvili <iraklijanashvili17@gmail.com>
2025-04-15 17:59:16 +0400

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   ],
   build: {
     rollupOptions: {
-      external: ['react-error-boundary']
+      external: ['react-error-boundary', '@supabase/supabase-js']
     },
   },
   resolve: {
